90f1dcedb0ecef1bf9997e2ec7bb25388e82bf99
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2023-12-13 12:37:58 +0900

    Title: 변수 값 교체하기 2, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231213/변수 값 교체하기 2/replacing-variable-values-2.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 5;
+        int b = 3;
+        b = a;
+        System.out.print(a);
+                System.out.print(b);
+
+    }
+}
