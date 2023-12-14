f770ba0f309ef2b24f30df493e305803add642c5
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2023-12-14 15:11:11 +0900

    Title: 살 수 있는 물건 2, Time: 172ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231214/살 수 있는 물건 2/things-able-to-buy-2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int price = sc.nextInt();
+        if(price >= 3000){
+            System.out.print("book");
+        }
+        else if(price >= 1000){
+            System.out.print("mask");
+        }
+        else{
+            System.out.print("pen");
+
+        }
+    }
+}
