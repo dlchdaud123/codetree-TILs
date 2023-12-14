7b5c2523f20b4e543a1216042462adad37e608cf
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2023-12-14 13:58:56 +0900

    Title: 체질량지수, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231214/체질량지수/bmi.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int kg = sc.nextInt();
+
+        int bmi = kg * 100 * 100 / (m * m);
+        System.out.println(bmi);
+
+        
+        if (bmi >= 25) {
+            System.out.println("Obesity");
+        }
+    }
+}
