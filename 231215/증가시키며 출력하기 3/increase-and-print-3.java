92bae12bb783b756993c1ad463e53a3520bff7ee
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2023-12-15 22:31:45 +0900

    Title: 증가시키며 출력하기 3, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231215/증가시키며 출력하기 3/increase-and-print-3.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int n = 10;
+        while(n < 27){
+            System.out.print(n+" ");
+            n += 2;
+        }
+    }
+}
