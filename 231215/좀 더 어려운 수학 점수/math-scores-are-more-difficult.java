92f95425bca934ca2229578b46dfe240313363d3
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2023-12-15 14:07:59 +0900

    Title: 좀 더 어려운 수학 점수, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231215/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int A_M = sc.nextInt();
+        int A_E = sc.nextInt();
+        int B_M = sc.nextInt();
+        int B_E = sc.nextInt();
+
+        if(A_M == B_M){
+            System.out.println(A_E > B_E ? "A" : "B");
+        }
+        else if(A_M > B_M){
+            System.out.println("A");
+        }
+        else{
+            System.out.println("B");
+        }
+
+    }
+}
