368abbab0d9adb7871c8b82efa8fb746a989c396
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2023-12-17 20:00:15 +0900

    Title: 369 게임, Time: 141ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 231217/369 게임/369-game.java
+++ 231217/369 게임/369-game.java
@@ -7,23 +7,14 @@ public class Main {
         int n = sc.nextInt();
 
         for(int i = 1; i <= n; i++){
-            if(i <= 10){ // n이 한 자리의 수
-                if(i % 3 == 0){
+            if(i % 3 == 0){
                 System.out.print(0+" ");
-                }
-                else
-                System.out.print(i+" ");
-
             }
-            else if(i % 10 == 0){
-                System.out.print(i+" ");
-            }
-            else if(i > 10){ // n이 두자리 수
-                if(i % 3 == 0 || (i / 10) % 3 == 0 || (i % 10) % 3 == 0){
+            else if((i / 10) % 3 == 0){
                 System.out.print(0+" ");
-                }
-                else
-                System.out.print(i+" ");
+            }            
+            else if((i % 10) % 3 == 0){
+                System.out.print(0+" ");
             }
 
             // 최대는 99 십의 자리 몫이 3의 배수거나 
