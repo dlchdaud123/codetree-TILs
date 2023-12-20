ca4aadd51b80ff500f3005faca35595ede5b4612
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2023-12-21 08:42:14 +0900

    Title: 1까지 나누기, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 231221/1까지 나누기/divide-by-1.java
+++ 231221/1까지 나누기/divide-by-1.java
@@ -5,20 +5,23 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int cnt = 1;
-        
+        int cnt = 0;
         for(int i = 1; i <= n; i++){
             //System.out.println("i = "+i);
             //System.out.println("n = "+n);
             n /= i;
-            
+            //System.out.println("n/i = "+n/i);
             cnt++;
             //System.out.println("cnt = "+cnt);
 
-            if(n <= 1){
+            if(n/i <= 1){
+                //System.out.println("n/i <= 1");
+                cnt++;
+                //System.out.println("cnt = "+cnt);
+                System.out.println(+cnt);
                 break;
             }
         }
-        System.out.print(cnt);
+        
     }
 }
