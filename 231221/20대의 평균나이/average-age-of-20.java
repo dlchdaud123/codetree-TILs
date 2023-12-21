c4549f9814ad52f7bc4a1dcd767f8fbfdc3c747f
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2023-12-21 09:41:45 +0900

    Title: 20대의 평균나이, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231221/20대의 평균나이/average-age-of-20.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int cnt = 0;
+
+        while(true){
+            int n = sc.nextInt();
+            if(20 > n || n > 29){
+                System.out.printf("%.2f", (double)sum/cnt);
+                break;
+            }
+            sum += n;
+            cnt++;
+             
+            
+        }
+    }
+}
