0165367cfa00a94afdc6751bfacc3a5d615b0ee8
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2023-12-21 09:08:50 +0900

    Title: 몫 출력하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231221/몫 출력하기/print-share.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+            int n = sc.nextInt();
+            int cnt = 0;
+            if(n % 2 == 0){
+                System.out.println(n/2);
+            }
+            cnt++;
+            if(cnt > 3){
+                break;
+            }
+            
+        }
+    }
+}
