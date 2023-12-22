6d078afd1fb663866b10d3a03577103cb4abdea4
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2023-12-22 11:54:50 +0900

    Title: 홀수로 이루어진 정사각형, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231222/홀수로 이루어진 정사각형/square-with-odd-numbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                System.out.print(2*j+9+2*(i-1)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// 11 12 13 14 15
