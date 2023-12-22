6551871dfc010dba5816429d15456045aec2a7ad
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2023-12-22 17:34:55 +0900

    Title: 구구단 만들기 2, Time: 151ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231222/구구단 만들기 2/print-multiplication-table-2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for(int i = a; i <= b+1; i++){
+            for(int j = b; j >= a; j--){
+                System.out.print(j+" * "+2*(i-1)+" = "+(j*2*(i-1)));
+                if(j != a){
+                    System.out.print(" / ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
