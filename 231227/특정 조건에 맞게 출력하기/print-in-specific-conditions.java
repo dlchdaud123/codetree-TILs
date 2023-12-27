f17d155fba8c8714ca0008e80321b5da77ae40a5
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2023-12-27 17:58:18 +0900

    Title: 특정 조건에 맞게 출력하기, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 231227/특정 조건에 맞게 출력하기/print-in-specific-conditions.java
+++ 231227/특정 조건에 맞게 출력하기/print-in-specific-conditions.java
@@ -8,11 +8,16 @@ public class Main {
 
         for(int i = 0; i < 10; i++){
             arr[i] = sc.nextInt();
+            if(arr[i] == 0){
+                break;
+            }
             if(arr[i] % 2 == 1){ // 홀수
                 System.out.print(arr[i] + 3+ " ");
             }
             else
                 System.out.print(arr[i] / 2+ " ");
+            
+            
         }
     }
 }
