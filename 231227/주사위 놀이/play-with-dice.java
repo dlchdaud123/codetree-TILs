357ddaa1683ce6f2f41ee50f673cfc9a8e7f8a5d
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2023-12-27 18:40:42 +0900

    Title: 주사위 놀이, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231227/주사위 놀이/play-with-dice.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[10];
+        int cnt_arr[] = new int[7];
+
+        for(int i = 0; i < 10; i++){
+            arr[i] = sc.nextInt();
+            cnt_arr[arr[i]]++;
+        }
+        for(int i = 1; i < 7; i++){
+            System.out.println(i +" - "+cnt_arr[i]);
+        }
+    }
+}
