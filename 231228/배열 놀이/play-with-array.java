f7d240cf55767054e157329de6936e3a48ce9add
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2023-12-28 11:24:55 +0900

    Title: 배열 놀이, Time: 183ms, Memory: 11MB, Status: Passed - CodeTree

--- 231228/배열 놀이/play-with-array.java
+++ 231228/배열 놀이/play-with-array.java
@@ -15,42 +15,44 @@ public class Main {
 
         for(int i = 0; i < q; i++){ // 3. 질문 q개 받기
             int q_number = sc.nextInt(); // 질문 받기
-            System.out.print(q_number+"번째 질문.");
+            // System.out.print(q_number+"번째 질문.");
 
             if(q_number == 1){ // 1 질문
                 int a = sc.nextInt();
-                System.out.print("a: "+a+" ");
-                System.out.print("답은 "+arr[a-1]);
+                // System.out.print(a);
+                System.out.print(arr[a-1]);
                 System.out.println();
             }
             else if(q_number == 2){ // 2 질문
                 int a = sc.nextInt();
-                System.out.print("a: "+a+" ");
+                // System.out.print(a);
                 for(int j = 0; j < n; j++){
-                    System.out.print("j: "+j+" ");
+                    // System.out.print(j);
                     if(arr[j] == a){ // 배열 안에 a 가 있다면
                         idx = j; // 인덱스를 j 로 바꾸기
+                        // System.out.print(idx);
                     }
 
                     if(idx == j){ // 인덱스가 바뀌였다면
-                        System.out.print("답은 "+(j+1)); // 출력하고
+                        System.out.print((j+1)); // 출력하고
                         System.out.println();
                         break; // 끝내기
                     }
                     else if(j == n - 1 && idx == -1){ // for 문이 끝까지 돌았다면 0출력
-                        System.out.print("답은 "+'0');
+                        System.out.print('0');
                         System.out.println();
                     }
                 }
                 a = 0;
+                idx = -1;
             }
             else{ // 3 질문
                 int a = sc.nextInt();
                 int b = sc.nextInt();
-                System.out.print("a: "+a+" ");
-                System.out.print("b: "+b+" ");
+                // System.out.print(a);
+                // System.out.print(b);
                 for(int j = a - 1; j < b; j++){
-                    System.out.print("답은 "+arr[j]+" ");
+                    System.out.print(arr[j]+" ");
                 }
                 System.out.println();
             }
