fecd36f59065a83afee7f506b376e79ae72b8d78
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2024-01-02 17:35:21 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 230ms, Memory: 13MB, Status: Passed - CodeTree

--- 240102/중복되지 않는 숫자 중 최대/max-of-unique-number.java
+++ 240102/중복되지 않는 숫자 중 최대/max-of-unique-number.java
@@ -5,7 +5,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int arr[] = new int[n];
+        int arr[] = new int[n+1];
         int max = Integer.MIN_VALUE;
         int cnt = 0;
 
@@ -13,9 +13,9 @@ public class Main {
             arr[i] = sc.nextInt();
         }
 
-        for(int i = 0; i < n; i++){ // 중복 검사
+        for(int i = 0; i <= n; i++){ // 중복 검사
             // System.out.println("i= "+i);
-            for(int j = 0; j < n; j++){ // 모든 값이랑 중복 비교
+            for(int j = 0; j <= n; j++){ // 모든 값이랑 중복 비교
                 // System.out.println("j= "+j);
                 if(i == j){ // 서로 인덱스가 같다면 j 하나 넘기
                     continue;
@@ -24,9 +24,9 @@ public class Main {
                     // System.out.println("arr[j]와 arr[i]가 같습니다");
                     break; // 검사 종료
                 }
-                if(j == n-1 && max < arr[i]){ // j 가 끝까지 돌았고 배열요소가 맥스보다 크면
+                if(j == n && max < arr[i]){ // j 가 끝까지 돌았고 배열요소가 맥스보다 크면
                     max = arr[i]; // 맥스는 배열요소로 변경
-                    // System.out.println("j == n-1 && max < arr[i]");
+                    // System.out.println("j == n && max < arr[i]");
                     // System.out.println("max = "+max);
                 }
             }
