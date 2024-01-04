bb41808a43b7b64c630c46d0c498ae6474bc658e
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2024-01-04 11:27:21 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240104/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        int arr_idx[] = new int[100];
+        int max = -1;
+        int max2 = 0;
+        int idx = n;
+        int cnt = 0;
+
+        for(int i = 1; i <= n; i++){ // 배열 받기
+            arr[i-1] = sc.nextInt();
+            // System.out.print(arr[i-1]);
+        }
+        // System.out.println();
+
+            for(int i = 1; i <= n; i++){ // 최대값 찾기, idx 까지
+                if(max < arr[i-1]){ // 현재의 최댓값보다 배열요소가 크면
+                    // System.out.println("i = "+i);
+                    max = arr[i-1]; // 현재의 최댓값은 배열요소
+                    idx = i;
+                    arr_idx[cnt] = idx;
+                    cnt++;
+                    // System.out.println("max = "+max+" idx = "+idx);
+                    // System.out.println("arr_idx = "+arr_idx[i-1]);
+                    
+                }
+            }
+            for(int i = cnt; i > 0; i--){
+                System.out.print(arr_idx[i-1]+" ");
+            }
+    }
+}
