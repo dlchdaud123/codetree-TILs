5af386ab3461545786acb2aabafcda1e60e3c0ac
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2024-01-06 12:33:18 +0900

    Title: 격자 반대로 채우기, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

--- 240106/격자 반대로 채우기/grid-anti-fill.java
+++ 240106/격자 반대로 채우기/grid-anti-fill.java
@@ -7,9 +7,11 @@ public class Main {
         int n = sc.nextInt();
         int arr[][] = new int[n][n];
         int num = 1;
+        int o_e = n % 2; // 홀수면 1 짝수면 0
 
         for(int j = n-1; j >= 0; j--){
-            if(j % 2 == 0){ // 
+            
+            if(j % 2 != o_e){ // 밑에서 위로
                 for(int i = n-1; i >= 0; i--){
                     arr[i][j] = num;
                     num += 1;
