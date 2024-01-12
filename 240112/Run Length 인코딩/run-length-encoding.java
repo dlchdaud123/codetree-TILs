865c8e77b8d264e25e3322a2a51a326dad3dc1d6
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2024-01-12 14:45:06 +0900

    Title: Run Length 인코딩, Time: 150ms, Memory: 11MB, Status: Passed - CodeTree

--- 240112/Run Length 인코딩/run-length-encoding.java
+++ 240112/Run Length 인코딩/run-length-encoding.java
@@ -3,18 +3,27 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String arr = sc.nextLine();
+        String encoded = "";
         int cnt = 1;
 
         for(int i = 1; i < arr.length(); i++){
             // System.out.println(arr.charAt(i));
+            char curr = arr.charAt(0);
             if(arr.charAt(i) == arr.charAt(i-1)){ //i-1 == i이면 cnt++
                 cnt++;
             }
             else{
-                System.out.println(arr.charAt(i-1)+""+cnt); //다르면 cnt = 1
+                encoded += arr.charAt(i-1);
+                encoded += Integer.toString(cnt);
+                // System.out.print(arr.charAt(i-1)+""+cnt); //다르면 cnt = 1
                 cnt = 1;
             }
         }
-        System.out.println(arr.charAt(arr.length()-1)+""+cnt);
+        encoded += arr.charAt(arr.length()-1);
+        encoded += Integer.toString(cnt);
+
+        System.out.println(encoded.length());
+        System.out.println(encoded);
+        // System.out.print(arr.charAt(arr.length()-1)+""+cnt);
     }
 }
