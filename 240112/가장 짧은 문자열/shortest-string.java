fa7916429c2602e723c94ebb2e8742ca18152d8c
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2024-01-12 11:02:13 +0900

    Title: 가장 짧은 문자열, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

--- 240112/가장 짧은 문자열/shortest-string.java
+++ 240112/가장 짧은 문자열/shortest-string.java
@@ -7,11 +7,31 @@ public class Main {
         String str2 = sc.next();
         String str3 = sc.next();
         int max = 0;
-        int min = 20;
+        int min = 100;
 
-        for(int i = 0; i < 3; i++){
-            
+        //최대값
+        if(max < str1.length()){
+            max = str1.length();
         }
-        
+        if(max < str2.length()){
+            max = str2.length();
+        }
+        if(max < str3.length()){
+            max = str3.length();
+        }
+
+        //최소값
+        if(min > str1.length()){
+            min = str1.length();
+        }
+        if(min > str2.length()){
+            min = str2.length();
+        }
+        if(min > str3.length()){
+            min = str3.length();
+        }
+
+        System.out.print(max-min);
+        // System.out.print(min);
     }
 }
