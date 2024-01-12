80ef4aae32642ad74e883f724a0e9bd584e764de
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2024-01-12 10:39:18 +0900

    Title: 문자열 두번 출력하기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 240112/문자열 두번 출력하기/print-string-twice.java
+++ 240112/문자열 두번 출력하기/print-string-twice.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
-        System.out.print(str);
+        System.out.println(str);
+        System.out.println(str);
     }
 }
