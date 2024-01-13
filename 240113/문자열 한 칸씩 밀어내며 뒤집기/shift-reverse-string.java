06989b8777bf2f989dc25bd290830786c063c03f
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2024-01-13 12:50:37 +0900

    Title: 문자열 한 칸씩 밀어내며 뒤집기, Time: 302ms, Memory: 21MB, Status: Passed - CodeTree

--- 240113/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.java
+++ 240113/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.java
@@ -3,11 +3,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next(); // 문자열
-        String new_str = "";
         int q = sc.nextInt(); // 요청의 개수
         int len = str.length();
 
-        for(int i = 0; i < q; i++){
+        for(int i = 0; i < q; i++){ // 문자열 갯수가 짝수일 경우
             int q_num = sc.nextInt();
             if(q_num == 1){ // 1 문자열 당기고 맨 앞에걸 뒤로
                 str = str.substring(1) + str.substring(0, 1);
@@ -18,16 +17,15 @@ public class Main {
                 System.out.println(str);
             }
             else{ // 3
+                char[] arr = str.toCharArray();
+                char temp;
                 for(int j = 0; j < len/2; j++){
-                    new_str += str.substring(len-(j+1), len-j);
+                    temp = arr[j];
+                    arr[j] = arr[len - j - 1];
+                    arr[len - j - 1] = temp;
                 }
-                for(int j = 0; j < len/2; j++){
-                    new_str += str.substring(len/2 - (j+1), len/2 - j);
-                }
-
-                System.out.println(new_str);
-                str = new_str;
-                new_str = "";
+                str = String.valueOf(arr);
+                System.out.println(str);
             }
         }
     }
