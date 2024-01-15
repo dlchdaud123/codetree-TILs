7f59df952e295fb3be05e1796d0e7df372e411fc
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2024-01-15 15:05:11 +0900

    Title: 미는 횟수, Time: 140ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240115/미는 횟수/number-of-pushes.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.next(); // 밀어야하는 문자열
+        String str2 = sc.next(); // 비교해야하는 문자열
+        int n = 0; // 밀어야하는 최솟값
+
+        for(int i = 0; i < str1.length(); i++){
+            str1 = str1.substring(1) + str1.substring(0, 1);
+            // System.out.println(str1);
+            n++;
+            if(str1.equals(str2)){
+                break;
+            }
+        }
+        System.out.print(n);
+        
+
+        
+    }
+}
