8d680f383e97606813327c3a4ddd967486545561
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2024-01-17 19:19:42 +0900

    Title: 함수를 이용한 윤년 판별, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240117/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+        if(isOddYear(y)){
+            System.out.print("true");
+        }
+        else{
+            System.out.print("false");
+        }
+    }
+    public static boolean isOddYear(int n) {
+        if(n % 4 == 0){
+            return true;
+        }
+        if(n % 4 == 0 && n % 100 == 0){
+            return false;
+        }
+        if(n % 4 == 0 && n % 100 == 0 && n % 400 == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
