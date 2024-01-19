477bc78e7ed1c469d158c23f1d8adcf3458216bf
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2024-01-19 14:59:38 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 139ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240119/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
+++ 240119/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -24,19 +24,18 @@ public class Main {
             }
             if(arr_n1[i] == arr_n2[0]){ // n1 배열이 n2 배열의 첫요소와 같으면
                 cnt = 0;
-                // System.out.println("arr_n1[i] = "+arr_n1[i]);
+                System.out.println("arr_n1[i] = "+arr_n1[i]);
                 for(int j = i; j <= n2_len; j++){ // 같은 부분부터 n2의 길이 만큼 비교
                     if(arr_n1[j] == arr_n2[cnt]){
-                        // System.out.println(arr_n1[j]+" "+arr_n2[cnt]);
+                        System.out.println(arr_n1[j]+" "+arr_n2[cnt]);
                         cnt++;
                         if(cnt == n2_len){ // n1,n2 같은 개수가 n2 길이 만큼이면 - > 부분수열
                             System.out.println("Yes");
-                            // System.out.println("cnt = "+cnt);
+                            System.out.println("cnt = "+cnt);
                             break;
                         }
                     }
                 }
-                
             }
             if(i == n1_len - 1 && cnt != n2_len){ //i 가 끝까지 돌았는데 cnt는 n2배열의 길이와 같이 않다면
                 System.out.print("No");
