6ceeb7f1b435245f0a81e386b02f5fdb0bac9d0c
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2024-01-23 12:37:55 +0900

    Title: 상품 코드, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240123/상품 코드/product-code.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class ProductInf{
+    String productName;
+    int productCode;
+
+    public ProductInf(){
+        this.productName = "codetree";
+        this.productCode = 50;
+    }
+    public ProductInf(String productName, int productCode){
+        this.productName = productName;
+        this.productCode = productCode;
+    }
+
+}
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        ProductInf productInf1 = new ProductInf();
+        String productName = sc.next();
+        int productCode = sc.nextInt();
+
+        ProductInf productInf2 = new ProductInf(productName, productCode);
+
+        System.out.println("product "+productInf1.productCode+" is "+productInf1.productName);
+        System.out.println("product "+productInf2.productCode+" is "+productInf2.productName);
+
+
+    }
+}
