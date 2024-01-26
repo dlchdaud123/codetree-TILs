3c376d2214c09e5a3a46c187582dd7edfc69efc4
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2024-01-26 19:32:16 +0900

    Title: 절댓값으로 변경, Time: 164ms, Memory: 10MB, Status: Passed - CodeTree

--- 240126/절댓값으로 변경/find-the-absolute-value.java
+++ 240126/절댓값으로 변경/find-the-absolute-value.java
@@ -1,21 +1,30 @@
 import java.util.Scanner;
 
 public class Main {
+    public static final int Max_N = 50;
+
+    public static int n;
+    public static int[] arr = new int[Max_N];
+
     public static void modify(int arr[]){
         for(int i = 0; i < arr.length; i++){
-            System.out.print(Math.abs(arr[i])+" ");
+            if(arr[i] < 0)
+                arr[i] = -arr[i];
         }
+        return;
         
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int arr[] = new int[n];
-        
+        int n = sc.nextInt(); 
         for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
         }
         modify(arr);
+
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i] + " ");
+        }
     }
 }
