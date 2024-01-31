1d00ea5c60504baaec1e1c1dd2e732cd05c7e0c3
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2024-01-31 10:51:06 +0900

    Title: 홀수 짝수에 따른 출력값, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 240131/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.java
+++ 240131/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 public class Main {
     public static int sum(int n){
+        if(n == 0) {
+            return 0;
+        }
         if(n == 1) {
             return 1;
         }
