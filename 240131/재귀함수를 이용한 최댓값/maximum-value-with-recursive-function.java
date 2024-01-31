a17645d00cbf9b62eaabeac996398f0ff444795f
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2024-01-31 11:45:53 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 241ms, Memory: 10MB, Status: Passed - CodeTree

--- 240131/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.java
+++ 240131/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.java
@@ -3,10 +3,10 @@ public class Main {
     public static Scanner sc = new Scanner(System.in);
     public static int n = sc.nextInt();
     public static int[] arr = new int[n];
-    public static int cnt = 0;
+    // public static int cnt = 0;
     public static int max_val = 0;
     
-    public static int max(int[] arr) {
+    public static int max(int[] arr, int cnt) {
         if(cnt == n){
             return max_val;
         }
@@ -14,12 +14,13 @@ public class Main {
             max_val = arr[cnt];
         }
         cnt++;
-        return max(arr);
+        return max(arr, cnt);
     }
     public static void main(String[] args) {
+        int cnt = 0;
         for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
         }
-        System.out.print(max(arr));
+        System.out.print(max(arr, cnt));
     }
 }
