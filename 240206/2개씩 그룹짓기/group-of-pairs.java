65e6304169c6cdee1d17bdbc4dfe9226329d6bdf
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2024-02-06 11:26:56 +0900

    Title: 2개씩 그룹짓기, Time: 213ms, Memory: 13MB, Status: Passed - CodeTree

--- 240206/2개씩 그룹짓기/group-of-pairs.java
+++ 240206/2개씩 그룹짓기/group-of-pairs.java
@@ -5,22 +5,28 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[2*n];
+        int max = 0;
+
         for(int i = 0; i < 2*n; i++){
             arr[i] = sc.nextInt();
         }
-        int[] arrSmall = new int[n];
 
-        // 오름 차순
-        Arrays.sort(arrSmall);
-        for(int i = 0; i < n; i++){
-            arrSmall[i] = arr[i];
+        Arrays.sort(arr);
+        if(n % 2 == 0){ // n이 짝수
+            for(int i = 0; i < n; i++) {
+                if(arr[i] + arr[2*n-1-i] > max){
+                    max = arr[i] + arr[2*n-1-i];
+                }
+            }
         }
-        // 내림 차순
-        Integer[] arrBig = Arrays.stream(arr).boxed().toArray(Integer[]::new);
-        Arrays.sort(arrBig, Collections.reverseOrder());
-        for(int i = n; i < 2*n; i++){
-            arrBig[i] = arr[i];
+        else{ // n이 홀수
+            for(int i = 0; i < n; i++) {
+                if(arr[i] + arr[2*n-1-i] > max){
+                    max = arr[i] + arr[2*n-1-i];
+                }
+            }
         }
-        System.out.print(arrSmall[0]+arrBig[0]);
+        
+        System.out.print(max);
     }
 }
