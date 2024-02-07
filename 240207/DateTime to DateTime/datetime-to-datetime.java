24baa9a61bd43dad41c5d6cb85296ce6116b44aa
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2024-02-07 09:57:08 +0900

    Title: DateTime to DateTime, Time: 139ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240207/DateTime to DateTime/datetime-to-datetime.java
+++ 240207/DateTime to DateTime/datetime-to-datetime.java
@@ -13,7 +13,10 @@ public class Main {
         int elapsedMin = -1;
 
         while(true) {
-            if(11 > hourB || 11 > minC){
+            if(11 > hourB && 11 > minC){
+                break;
+            }
+            if(11 > hourB){
                 break;
             }
             if(day == dayA && hour == hourB && min == minC)
