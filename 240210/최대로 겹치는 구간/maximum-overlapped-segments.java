df5edb9c8ad599994bbd011e3601579ce9d802fb
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2024-02-10 21:44:19 +0900

    Title: 최대로 겹치는 구간, Time: 158ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240210/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[200];
+        int a = 0;
+        int b = 0;
+        int max = 0;
+        int cnt = 0;
+
+        
+        for(int i = 0; i < n; i++){
+            a = sc.nextInt()+100;
+            b = sc.nextInt()+100;
+            for(int j = a; j < b; j++){
+                arr[i] += 1;
+            }
+        }
+        for(int i = 0; i < 200; i++) {
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        for(int i = 0; i < 200; i++) {
+            if(arr[i] == max) {
+                cnt++;
+            }
+            System.out.print(arr[i]+" ");
+        }
+        // System.out.print(cnt);
+    }
+}
