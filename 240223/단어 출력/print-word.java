d55222d9830a82923c3d40816040cb218e8f68f3
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2024-02-23 14:12:44 +0900

    Title: 단어 출력, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240223/단어 출력/print-word.java
+++ 240223/단어 출력/print-word.java
@@ -2,27 +2,31 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int x = sc.nextInt();
-    int y = sc.nextInt();
-    int[] month = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    int days = 0;
-    // 월 차를 이용해서 일수 구하기
-    // 일 차를 이용해서 일수 구하기
-    // 일 수를 7로 나눈 나머지를 이용해서 요일 알아내기
-
-    // x가 1월이면 일 차만 이용해서 일수 구하기
-    if(x == 1){
-      for(int i = 1; i < y; i++){
-        days++;
-      }
+    int N = sc.nextInt();
+    int[] arr = new int[N];
+    for(int i = 0; i < N; i++){
+      arr[i] = sc.nextInt();
     }
-    // x가 2월이상이면 월차, 일차 같이 구하기
-    else
-      for(int i = 1; i < x; i++){
-        days += month[i];
+    int M = sc.nextInt();
+    int[] arr2 = new int[M];
+    for(int i = 0; i < M; i++){
+      arr2[i] = sc.nextInt();
+    }
+    boolean exist = false;
+    for(int i = 0; i < M; i++){
+      for(int j = 0; j < N; j++){
+        if(arr2[i] == arr[j]){
+          exist = true;
+          continue;
+        }
       }
-      days += y;
+      if(exist == true){
+        System.out.println(1+" ");
+      }
+      else{
+        System.out.println(0+" ");
+      }
+      exist = false;
+    }
   }
-  String[] week = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
-  System.out.print(days % 7);
 }
