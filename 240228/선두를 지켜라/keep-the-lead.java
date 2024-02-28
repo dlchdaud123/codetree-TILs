279a0b25cb7b43d2b0c4fba90ea3c3f61d2fd965
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2024-02-28 10:50:17 +0900

    Title: 선두를 지켜라, Time: 233ms, Memory: 28MB, Status: Wrong Answer - CodeTree

---
+++ 240228/선두를 지켜라/keep-the-lead.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[] arrA = new int[1000000];
+        int[] arrB = new int[1000000];
+
+        int totalTime = 0;
+        int cnt = 0;
+
+        for(int i = 0; i < n; i++){
+            int v = sc.nextInt();
+            int t = sc.nextInt();
+            for(int j = 0; j < t; j++){
+                cnt += v;
+                arrA[totalTime++] = cnt;
+            }
+        }
+        totalTime = 0;
+        cnt = 0;
+
+        for(int i = 0; i < m; i++){
+            int v = sc.nextInt();
+            int t = sc.nextInt();
+            for(int j = 0; j < t; j++){
+                cnt += v;
+                arrB[totalTime++] = cnt;
+            }
+            
+        }
+
+        // for(int i = 0; i < totalTime; i++){
+        //     System.out.println(arrA[i] + " " + arrB[i]);
+        // }
+        int change = 0;
+        int curr = 0;;
+        int[] diff = new int[totalTime];
+        for(int i = 0; i < totalTime; i++){
+            diff[i] = (arrA[i] - arrB[i]);
+            // System.out.println(diff[i]);
+            if(diff[i] * curr < 0){
+                change++;
+            }
+            if(diff[i] != 0){
+                curr = diff[i];
+            }
+        }
+        System.out.print(change);
+        
+
+    }
+}
