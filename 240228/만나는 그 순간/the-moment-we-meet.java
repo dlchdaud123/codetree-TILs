a4338cdc4de225fac72bcfca44cc932998c5cc68
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2024-02-28 10:02:37 +0900

    Title: 만나는 그 순간, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240228/만나는 그 순간/the-moment-we-meet.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[] arrA = new int[1000];
+        int[] arrB = new int[1000];
+        int totalTime = 0;
+        int cnt = 0;
+
+        for(int i = 0; i < n; i++){
+            String dir = sc.next();
+            int time = sc.nextInt();
+            if(dir.equals("R")){
+                for(int j = 0; j < time; j++){
+                    arrA[totalTime++] = cnt++;
+                }
+            }
+            else{
+                for(int j = 0; j < time; j++){
+                    arrA[totalTime++] = cnt--;
+                }
+            }
+            
+        }
+        totalTime = 0;
+        cnt = 0;
+        for(int i = 0; i < m; i++){
+            String dir = sc.next();
+            int time = sc.nextInt();
+            if(dir.equals("R")){
+                for(int j = 0; j < time; j++){
+                    arrB[totalTime++] = cnt++;
+                }
+            }
+            else{
+                for(int j = 0; j < time; j++){
+                    arrB[totalTime++] = cnt--;
+                }
+            }
+        }
+
+        // for(int i = 0; i < 1000; i++){
+        //     System.out.println(arrA[i]+" "+arrB[i]);
+        // }
+        for(int i = 1; i < 1000; i++){
+            if(arrA[i] == arrB[i]){
+                System.out.print(i);
+                return;
+            }
+        }
+    }
+}
