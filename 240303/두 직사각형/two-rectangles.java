b212b1a1736ec3b60375e8acf9e71b0924ccbd0e
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2024-03-03 23:39:59 +0900

    Title: 두 직사각형, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240303/두 직사각형/two-rectangles.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+        int a1 = sc.nextInt();
+        int b1 = sc.nextInt();
+        int a2 = sc.nextInt();
+        int b2 = sc.nextInt();
+
+        if(x2 < a1){
+            System.out.print("nonoverlapping");
+        }
+        else if(a2 < x1){
+            System.out.print("nonoverlapping");
+        }
+        else if(b2 < y1){
+            System.out.print("nonoverlapping");
+        }
+        else if(y2 < b1){
+            System.out.print("nonoverlapping");
+        }
+        else{
+            System.out.print("overlapping");
+        }
+    }
+}
