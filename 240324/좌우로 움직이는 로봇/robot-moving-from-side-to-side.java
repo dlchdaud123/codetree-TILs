017c35e0c0fa05d11c94e39ea631595938654959
dlchdaud123 <53869165+dlchdaud123@users.noreply.github.com>
2024-03-24 23:53:40 +0900

    Title: 좌우로 움직이는 로봇, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240324/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int arr1[] = new int[100000];
+        int arr2[] = new int[100000];
+        int nCurr = 0;
+        int mCurr = 0;
+        int nCurrIdx = 0;
+        int mCurrIdx = 0;
+
+        for(int i = 0; i < n; i++){
+            int time = sc.nextInt();
+            String dir = sc.next();
+            if(dir.equals("L")){
+                for(int j = 0; j < time; j++){
+                    arr1[nCurrIdx++] = nCurr--; 
+                }
+            }
+            else{
+                for(int j = 0; j < time; j++){
+                    arr1[nCurrIdx++] = nCurr++; 
+                }
+            }
+        }
+
+        for(int i = 0; i < m; i++){
+            int time = sc.nextInt();
+            String dir = sc.next();
+            if(dir.equals("L")){
+                for(int j = 0; j < time; j++){
+                    arr2[mCurrIdx++] = mCurr--; 
+                }
+            }
+            else{
+                for(int j = 0; j < time; j++){
+                    arr2[mCurrIdx++] = mCurr++; 
+                }
+            }
+        }
+        int meetCnt = 0;
+        if(mCurrIdx > nCurrIdx){
+            for(int i = 0; i < mCurrIdx; i++){
+                // System.out.println(i+ " "+ arr1[i] + " " + arr2[i]);
+                if(arr1[i] == arr2[i]){
+                    meetCnt++;
+                }
+            }
+        }
+        else{
+            for(int i = 0; i < nCurrIdx; i++){
+                // System.out.println(i+ " "+ arr1[i] + " " + arr2[i]);
+                if(arr1[i] == arr2[i]){
+                    meetCnt++;
+                }
+            }
+        }
+        System.out.println(meetCnt - 1);
+    }
+}
