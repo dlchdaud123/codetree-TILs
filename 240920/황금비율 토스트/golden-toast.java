f7f342453881f84f5179915eafc5829a6c1a20b1
CHONGMYOUNG LEE <53869165+dlchdaud123@users.noreply.github.com>
2024-09-20 20:06:13 +0900

    Title: 황금비율 토스트, Time: 1125ms, Memory: 35MB, Status: Passed - CodeTree

--- 240920/황금비율 토스트/golden-toast.java
+++ 240920/황금비율 토스트/golden-toast.java
@@ -1,35 +1,52 @@
+import java.util.Scanner;
 import java.util.LinkedList;
 import java.util.ListIterator;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        LinkedList<Character> l = new LinkedList<>(); // 문자를 관리할 list를 선언합니다. => 빈 이중 연결 리스트
-        int n = sc.nextInt(); // 식빵 갯수
-        int m = sc.nextInt(); // 명령어 갯수
+    public static int n, m;
+    public static String s;
 
-        for(int i = 0; i < n; i++){
-            l.add(sc.next());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 입력:
+        n = sc.nextInt();
+        m = sc.nextInt();
+        s = sc.next();
+
+        // 연결리스트 정의
+        LinkedList<Character> l = new LinkedList<>();
+        for(int i = 0; i < s.length(); i++)
+			l.add(s.charAt(i));
+        // Iterator 정의 (맨 끝 위치에서 시작하기 위해서는 listIterator안에 l.size() 필요)
+        ListIterator<Character> it = l.listIterator(l.size());
+
+        while(m-- > 0) {
+            char command = sc.next().charAt(0);
+
+            if(command == 'L') {
+                if(it.hasPrevious()) // 빵들의 맨 앞이 아니라면
+                    it.previous();   // 앞으로 이동합니다.
+            } 
+            else if(command == 'R') {
+                if(it.hasNext())     // 빵들의 맨 뒤가 아니라면
+                    it.next();       // 뒤로 이동합니다.
+            }
+            else if(command == 'D') {
+                if(it.hasNext()) {   // 빵들의 맨 뒤가 아니라면
+                    it.next();
+                    it.remove();     // 바로 뒤에 있는 빵을 제거합니다.
+                }
+            }
+            else if(command == 'P') {
+                char c = sc.next().charAt(0);
+                it.add(c);           // 가리키는 위치에 문자 c를 추가합니다.
+            }    
         }
 
-        // iterator를 list의 맨 앞에 위치시킵니다.
-        ListIterator<Character> it;
-
-        // iterator를 list의 맨 앞에 위치시킵니다.
+        // 출력:
         it = l.listIterator();
-        System.out.println(it.next());      // 원소 값을 출력하며 한 칸 뒤로 이동합니다.  ('a')
-        System.out.println(it.next());      // 원소 값을 출력하며 한 칸 뒤로 이동합니다.  ('b')
-        System.out.println(it.previous());  // 원소 값을 출력하며 한 칸 앞으로 이동합니다. ('b')
-        System.out.println(it.previous());  // 원소 값을 출력하며 한 칸 앞으로 이동합니다. ('a')
-
-        it.next();                          // remove 전에는 next가 필요합니다.
-        it.remove();                        // 원소 'a'를 제거합니다.
-        it.add('d');                        // 원소 'd'를 추가합니다.
-        
-        // list에 들어있는 원소 값을 순서대로 출력합니다.
-        it = l.listIterator();
-        while(it.hasNext()) {               // 'd' 'b' 'c'
-            System.out.print(it.next());    // 원소 값을 출력하며 한 칸 뒤로 이동합니다.
-        } 
+        while(it.hasNext())
+            System.out.print(it.next());
     }
 }
