d9be8ab8088f2919f553fcb263681652f9c49edd
CHONGMYOUNG LEE <53869165+dlchdaud123@users.noreply.github.com>
2024-10-22 15:09:16 +0900

    Title: 정수 명령 처리, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241022/정수 명령 처리/process-numeric-commands.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i < n; i++){
+            String s = sc.next();
+            if(s.equals("push")){
+                int j = sc.nextInt();
+                stack.push(j);
+            }
+            else if(s.equals("pop")){
+                System.out.println(stack.pop());
+            }
+            else if(s.equals("size")){
+                System.out.println(stack.size());
+            }
+            else if(s.equals("empty")){
+                if(stack.empty()){
+                    System.out.println(1);
+                }
+                else{
+                    System.out.println(0);
+                }
+            }
+            else if(s.equals("top")){
+                stack.peek();
+            }
+        }
+    }
+}
