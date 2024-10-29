53235d745a635eec54f554fee35eb3739de94175
CHONGMYOUNG LEE <53869165+dlchdaud123@users.noreply.github.com>
2024-10-29 19:36:28 +0900

    Title: 원형 순열에서의 인원 제거, Time: 547ms, Memory: 79MB, Status: Passed - CodeTree

---
+++ 241029/원형 순열에서의 인원 제거/josephus-permutations.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        Queue <Integer> q = new LinkedList<>();
+
+        for(int i = 1; i <= n; i++){
+            q.add(i);
+        }
+
+        while(q.size() != 1) {
+            for(int i = 0; i < k - 1; i++){
+                q.add(q.poll());
+            }
+
+            System.out.print(q.poll() + " ");
+        }
+
+        System.out.print(q.peek());
+        
+    }
+}
