ea2ccef74cb373402330f5915677c83a5b1935bd
CHONGMYOUNG LEE <53869165+dlchdaud123@users.noreply.github.com>
2024-10-30 12:07:23 +0900

    Title: 수열 조작, Time: 211ms, Memory: 21MB, Status: Passed - CodeTree

--- 241030/수열 조작/sequence-manipulation.java
+++ 241030/수열 조작/sequence-manipulation.java
@@ -8,11 +8,11 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        for(int i = 0; i < n; i++){
+        for(int i = 1; i <= n; i++){
             dq.addLast(i);
         }
 
-        while(dq.size() == 1){
+        while(dq.size() != 1){
             dq.pollFirst();
             dq.addLast(dq.peekFirst());
             dq.pollFirst();
@@ -20,3 +20,31 @@ public class Main {
         System.out.println(dq.peekLast());
     }
 }
+
+// deque를 이용할 때 자주 사용되는 것은 다음 8가지 입니다.
+
+// addFirst(E)
+// 맨 앞에 데이터 E를 추가합니다.
+
+// addLast(E)
+// 맨 뒤에 데이터 E를 추가합니다.
+
+// pollFirst()
+// 맨 앞에 있는 데이터를 반환합니다. 동시에 그 데이터를 deque에서 뺍니다.
+
+// pollLast()
+// 맨 뒤에 있는 데이터를 반환합니다. 동시에 그 데이터를 deque에서 뺍니다.
+
+// size()
+// 현재 deque에 들어있는 데이터의 수를 반환합니다.
+
+// isEmpty()
+// 현재 deque가 비어있다면 true, 아니라면 false를 반환합니다.
+
+// peekFirst()
+// deque의 맨 앞에 있는 데이터를 반환합니다.
+
+// peekLast()
+// deque의 맨 뒤에 있는 데이터를 반환합니다.
+
+// 따라서 다음과 같이 코드를 작성해볼 수 있습니다.
